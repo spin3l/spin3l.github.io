38bfd62b04b208c77bd78dbc7c8fbb7705a527aa
Christian López García <68792480+drownedd@users.noreply.github.com>
2025-01-11 13:03:19 +0100

    Fixed background and change some things

---
+++ src/constants/tags.ts
@@ -0,0 +1,60 @@
+import AngularIcon from "../components/icons/AngularIcon.astro";
+import DjangoIcon from "../components/icons/DjangoIcon.astro";
+import JavaIcon from "../components/icons/JavaIcon.astro";
+import PythonIcon from "../components/icons/PythonIcon.astro";
+import SpringIcon from "../components/icons/SpringIcon.astro";
+
+const TAGS = {
+  DJANGO: {
+    name: "Django",
+    class: "bg-[#145A32] text-white",
+    icon: DjangoIcon,
+  },
+  PYTHON: {
+    name: "Python",
+    class: "bg-[#1E4B73] text-white",
+    icon: PythonIcon,
+  },
+  JAVA: {
+    name: "Java",
+    class: "bg-[#ADD8E6] text-white",
+    icon: JavaIcon,
+  },
+  ANGULAR: {
+    name: "AngularJS",
+    class: "bg-[#B52E31] text-white",
+    icon: AngularIcon,
+  },
+  CVAT: {
+    name: "CVAT",
+    class: "bg-[#CC7A1E] text-white",
+    icon: undefined,
+  },
+  SPRING: {
+    name: "Spring",
+    class: "bg-[#3A6F22] text-white",
+    icon: SpringIcon,
+  },
+  PYTORCH: {
+    name: "PyTorch",
+    class: "bg-[#EE4C2C] text-white",
+    icon: undefined,
+  },
+  SKLEARN: {
+    name: "Scikit-Learn",
+    class: "bg-[#C76A0E] text-white",
+    icon: undefined,
+  },
+  TKINTER: {
+    name: "Tkinter",
+    class: "bg-[#CC9A06] text-white",
+    icon: undefined,
+  },
+  BOOTSTRAP: {
+    name: "Bootstrap",
+    class: "bg-[#7952B3] text-white",
+    icon: undefined,
+  },
+};
+
+export default TAGS;
