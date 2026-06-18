e75936af389582a35a7d26ab39fb279e05756b48
spin3l <dev.christianlopezgarcia@gmail.com>
2026-06-18 10:12:05 +0200

    impl: i18n & theming

--- src/components/CopyClipboard.tsx
+++ src/components/CopyClipboard.tsx
@@ -1,14 +1,20 @@
+import { getLangFromUrl, useTranslations } from "@/i18n/utils.ts";
 import { createRef, type ComponentChildren } from "preact";
 import { ClipboardIcon } from "./icons/ClipboardIcon.tsx";
 
 export const CopyClipboard = ({
+  url,
   children,
   ...props
 }: {
+  url: URL;
   children: ComponentChildren;
 }) => {
   const snippetRef = createRef<HTMLPreElement>();
   const buttonRef = createRef<HTMLButtonElement>();
+
+  const lang = getLangFromUrl(url);
+  const t = useTranslations(lang);
 
   const copyToClipboard = async () => {
     let snippet = snippetRef.current;
@@ -19,28 +25,28 @@ export const CopyClipboard = ({
     await navigator.clipboard.writeText(snippetText);
 
     // Apply feedback
-    copyFeedback.innerText = "Copiado!";
+    copyFeedback.innerText = t("actions.copied");
     button.classList.remove("bg-gray-200");
     button.classList.add("bg-green-200");
 
     setTimeout(() => {
-      copyFeedback.innerText = "Copiar";
+      copyFeedback.innerText = t("actions.copy");
       button.classList.remove("bg-green-200");
       button.classList.add("bg-gray-200");
     }, 700);
   };
 
   return (
-    <div className="flex flex-wrap gap-4 items-center justify-center ">
-      <pre {...props} ref={snippetRef} class="text-lg  md:text-xl">
+    <div className="flex flex-wrap justify-center items-center gap-4">
+      <pre {...props} ref={snippetRef} class="text-lg md:text-xl">
         {children}
       </pre>
       <button
-        className="flex justify-center items-center w-full sm:w-[fit-content] p-1 gap-x-2 text-sm text-gray-800 bg-gray-200 rounded-lg top-4 right-4 px-2"
+        className="top-4 right-4 flex justify-center items-center gap-x-2 bg-gray-200 p-1 px-2 rounded-lg w-full sm:w-[fit-content] text-gray-800 text-sm"
         ref={buttonRef}
         onClick={copyToClipboard}
       >
-        <ClipboardIcon /> <p id="copy-text">Copiar</p>
+        <ClipboardIcon /> <p id="copy-text">{t("actions.copy")}</p>
       </button>
     </div>
   );
---
+++ src/i18n/ui.ts
@@ -0,0 +1,207 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+  ca: "Català",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    title: "spin3l portfolio - Full Stack Engineer",
+    description:
+      'Christian\'s "spin3l" López García portfolio - Remote Full Stack Engineer, Spain',
+    me: "Christian López García",
+
+    "actions.repository": "Repository",
+    "actions.preview": "Preview",
+    "actions.package": "Package",
+    "actions.copy": "Copy",
+    "actions.copied": "Copied!",
+    "actions.download_cv": "CV",
+
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.open-source": "Open Source",
+    "nav.contact": "Contact",
+
+    "nav.footer.rights": "Almost all rights reserved, inspired in the works of",
+
+    "section.hero.greet": "Hey! It's",
+    "section.hero.role": "Full Stack Engineer",
+    "section.hero.based": "based in",
+    "section.hero.country": "Spain",
+    "section.hero.description": "Future-proof React and Python",
+
+    "section.projects.title": "Projects",
+    "section.open-source.title": "Open-Source Contributions",
+    "section.experience.title": "Work Experience",
+    "section.experience.agrospai.date": "January 2025 - Present",
+    "section.experience.agrospai.title": "AgrospAI",
+    "section.experience.agrospai.company": "University of Lleida",
+    "section.experience.agrospai.description":
+      "Technical staff on the AgrospAI project, one of Spain's four agri-food sector data spaces. Full-stack development of marketplace features, web3, design and development of the portal, and implementation of use cases through Dataspace algorithms.",
+
+    "section.experience.ecityclic.date": "November - December 2024",
+    "section.experience.ecityclic.title": "Junior Software Developer",
+    "section.experience.ecityclic.company": "eCityclic",
+    "section.experience.ecityclic.description":
+      "Full-stack development of applications in the e-government sector.",
+
+    "section.experience.gft.date": "March - July 2024",
+    "section.experience.gft.title": "Data Scientist Intern",
+    "section.experience.gft.company": "GFT IT Consulting S.L.U.",
+    "section.experience.gft.description":
+      "Participation in Natural Language Processing and Computer Vision projects, as well as dataset preparation and development of technical demonstrations.",
+
+    "section.experience.udl.date": "June - December 2023",
+    "section.experience.udl.company": "University of Lleida",
+    "section.experience.udl.title": "Technical Staff",
+    "section.experience.udl.description":
+      "Design and development of applications, data processing, and model training for hyperspectral image analysis in the agri-food sector.",
+
+    "section.projects.2chess.description":
+      "Browser-based chess for two local players",
+    "section.projects.2chess.description.view-and-review.description":
+      "Web app to see the newest movie releases, review them and create watch-lists",
+
+    "section.contributions.fastrag.description":
+      "CLI tool that leverages the execution of RAG generation and testing workflows using declarative configuration files. Auto-populates vectorstore with generated chunks, provides a serving API with automatic monitoring and dashboard.",
+    "section.contributions.ocean-runner.description":
+      "Ocean Runner is a Python package that eases algorithm creation in the scope of OceanProtocol.",
+    "section.contributions.oceanprotocol-job-details.description":
+      "OceanProtocol Job Details is a Python package to get details from within OceanProtocol jobs.",
+  },
+  es: {
+    title: "Portfolio de spin3l - Ingeniero Full Stack",
+    description:
+      "Portfolio de Christian 'spin3l' López García - Ingeniero Full Stack en remoto, España",
+    me: "Christian López García",
+
+    "actions.repository": "Repositorio",
+    "actions.preview": "Vista previa",
+    "actions.package": "Paquete",
+    "actions.copy": "Copiar",
+    "actions.copied": "¡Copiado!",
+    "actions.download_cv": "CV",
+
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.open-source": "Código abierto",
+    "nav.contact": "Contacto",
+
+    "nav.footer.rights":
+      "Casi todos los derechos reservados, inspirado en los trabajos de",
+
+    "section.hero.greet": "¡Hola! Soy",
+    "section.hero.role": "Ingeniero Full Stack",
+    "section.hero.based": "en",
+    "section.hero.country": "Lleida, España",
+    "section.hero.description": "Future-proof React y Python",
+
+    "section.projects.title": "Proyectos",
+    "section.open-source.title": "Contribuciones de código abierto",
+    "section.experience.title": "Experiencia laboral",
+    "section.experience.agrospai.date": "Enero de 2025 - Presente",
+    "section.experience.agrospai.title": "AgrospAI",
+    "section.experience.agrospai.company": "Universidad de Lleida",
+    "section.experience.agrospai.description":
+      "Personal técnico en el proyecto AgrospAI, uno de los cuatro espacios de datos del sector agroalimentario en España. Desarrollo full-stack de funcionalidades del marketplace, web3, diseño y desarrollo del portal, e implementación de casos de uso mediante algoritmos de Dataspace.",
+
+    "section.experience.ecityclic.date": "Noviembre - Diciembre de 2024",
+    "section.experience.ecityclic.title": "Desarrollador de Software Junior",
+    "section.experience.ecityclic.company": "eCityclic",
+    "section.experience.ecityclic.description":
+      "Desarrollo full-stack de aplicaciones en el sector de la administración electrónica.",
+
+    "section.experience.gft.date": "Marzo - Julio de 2024",
+    "section.experience.gft.title": "Científico de Datos en Prácticas",
+    "section.experience.gft.company": "GFT IT Consulting S.L.U.",
+    "section.experience.gft.description":
+      "Participación en proyectos de Procesamiento del Lenguaje Natural y Visión por Computador, así como preparación de conjuntos de datos y desarrollo de demostraciones técnicas.",
+
+    "section.experience.udl.date": "Junio - Diciembre de 2023",
+    "section.experience.udl.company": "Universidad de Lleida",
+    "section.experience.udl.title": "Personal Técnico",
+    "section.experience.udl.description":
+      "Diseño y desarrollo de aplicaciones, procesamiento de datos y entrenamiento de modelos para el análisis de imágenes hiperespectrales en el sector agroalimentario.",
+
+    "section.projects.2chess.description":
+      "Ajedrez en el navegador para dos jugadores locales",
+    "section.projects.2chess.description.view-and-review.description":
+      "Aplicación web para ver los últimos estrenos de cine, valorarlos y crear listas de seguimiento",
+
+    "section.contributions.fastrag.description":
+      "Herramienta de CLI que optimiza la ejecución de flujos de trabajo de generación y prueba de RAG mediante archivos de configuración declarativos. Alimenta automáticamente el vectorstore con los fragmentos generados y proporciona una API de servicio con monitorización automática y panel de control.",
+    "section.contributions.ocean-runner.description":
+      "Ocean Runner es un paquete de Python que facilita la creación de algoritmos en el ámbito de OceanProtocol.",
+    "section.contributions.oceanprotocol-job-details.description":
+      "OceanProtocol Job Details es un paquete de Python para obtener detalles internos de las tareas de OceanProtocol.",
+  },
+  ca: {
+    title: "Portfoli de spin3l - Enginyer Full Stack",
+    description:
+      "Portfoli de Christian 'spin3l' López García - Enginyer Full Stack en remot, Espanya",
+    me: "Christian López García",
+
+    "actions.repository": "Repositori",
+    "actions.preview": "Vista prèvia",
+    "actions.package": "Paquet",
+    "actions.copy": "Copiar",
+    "actions.copied": "Copiat!",
+    "actions.download_cv": "CV",
+
+    "nav.experience": "Experiència",
+    "nav.projects": "Projectes",
+    "nav.open-source": "Codi obert",
+    "nav.contact": "Contacte",
+
+    "nav.footer.rights":
+      "Gairebé tots els drets reservats, inspirat en els treballs de",
+
+    "section.hero.greet": "Hola! Soc",
+    "section.hero.role": "Enginyer Full Stack",
+    "section.hero.based": "establert a",
+    "section.hero.country": "Lleida, Espanya",
+    "section.hero.description": "Future-proof React i Python",
+
+    "section.projects.title": "Projectes",
+    "section.open-source.title": "Contribucions de codi obert",
+    "section.experience.title": "Experiència laboral",
+    "section.experience.agrospai.date": "Gener de 2025 - Actualitat",
+    "section.experience.agrospai.title": "AgrospAI",
+    "section.experience.agrospai.company": "Universitat de Lleida",
+    "section.experience.agrospai.description":
+      "Personal tècnic en el projecte AgrospAI, un dels quatre espais de dades del sector agroalimentari a Espanya. Desenvolupament full-stack de funcionalitats del marketplace, web3, disseny i desenvolupament del portal, i implementació de casos dús mitjançant algoritmes de Dataspace.",
+
+    "section.experience.ecityclic.date": "Novembre - Desembre de 2024",
+    "section.experience.ecityclic.title": "Desenvolupador de Programari Júnior",
+    "section.experience.ecityclic.company": "eCityclic",
+    "section.experience.ecityclic.description":
+      "Desenvolupament full-stack d'aplicacions en el sector de l'administració electrònica.",
+
+    "section.experience.gft.date": "Març - Juliol de 2024",
+    "section.experience.gft.title": "Científic de Dades en Pràctiques",
+    "section.experience.gft.company": "GFT IT Consulting S.L.U.",
+    "section.experience.gft.description":
+      "Participació en projectes de Processament del Llenguatge Natural i Visió per Computador, així com preparació de conjunts de dades i desenvolupament de demostracions tècniques.",
+
+    "section.experience.udl.date": "Juny - Desembre de 2023",
+    "section.experience.udl.company": "Universitat de Lleida",
+    "section.experience.udl.title": "Personal Tècnic",
+    "section.experience.udl.description":
+      "Disseny i desenvolupament d'aplicacions, processament de dades i entrenament de models per a l'anàlisi d'imatges hiperespectrals en el sector agroalimentari.",
+
+    "section.projects.2chess.description":
+      "Escacs al navegador per a dos jugadors locals",
+    "section.projects.2chess.description.view-and-review.description":
+      "Aplicació web per a veure les últimes novetats de cinema, valorar-les i crear llistes de reproducció",
+
+    "section.contributions.fastrag.description":
+      "Eina de CLI que optimitza l'execució de fluxos de treball de generació i prova de RAG mitjançant fitxers de configuració declaratius. Alimenta automàticament el vectorstore amb els fragments generats i proporciona una API de servei amb monitoratge automàtic i panell de control.",
+    "section.contributions.ocean-runner.description":
+      "Ocean Runner és un paquet de Python que facilita la creació d'algoritmes a l'àmbit d'OceanProtocol.",
+    "section.contributions.oceanprotocol-job-details.description":
+      "OceanProtocol Job Details és un paquet de Python per a obtenir detalls interns de les tasques d'OceanProtocol.",
+  },
+} as const;
